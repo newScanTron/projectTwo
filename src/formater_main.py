fdf50805d3992bb9187021142cdfa8d45fad8aeb
Christopher Murphy <christmurphy@gmail.com>
2014-10-06 20:52:04 -0600

    ive got the formating down, except the string is one long single word

--- src/formater_main.py
+++ src/formater_main.py
@@ -4,44 +4,50 @@ i have added the ability to have key="value"
 added to tags for the extra credit
 '''
 __author__ = 'newScanTron'
-print("what is up")
 xml_file = open('XML.txt', 'r+')
 read_xml = xml_file.read()
 xml_file_list = []
-space_num = []
 #going to  use this tag_list to know how many indents to use
 tag_list = []
 words_list = []
-string_two = ""
+string_string = ""
 #this regEx checks for <tag> | <tag key="value"> and their equevelient
 #closeing tags
 xml_file_list = re.split('(<[\w\s]+?>|<[\w\s]+?[=\s\*\?]"[\w\s]+?">|<[/\w\s]+?>)', read_xml)
 
+
 def spacer(num_of_spaces):
     """
     thinking this might be the way to add the appropriate
-    white space also recurssion cus we are talking about it
+    white space also recurssion cus we are talking about it. for some
+    reason that does not work right so a while loop is less stupid to fix
     :param num_of_spaces:
     :return:
     """
-    if num_of_spaces > 1:
-        space_num.append("    ")
+    space_num = ""
+    # if num_of_spaces > 1:
+    #     space_num += "    "
+    #     num_of_spaces -= 1
+    #     spacer(num_of_spaces)
+    while num_of_spaces > 1:
+        space_num += "    "
         num_of_spaces -= 1
-        spacer(num_of_spaces)
     return space_num
-
 
 for string_one in xml_file_list:
     if re.match('<[\w\s]+?>|<[\w\s]+?[=\s\*\?]"[\w\s]+?">', string_one):
-        print(string_one.rjust(len(tag_list)*12, ' '))
         tag_list.append(string_one)
+        print(spacer(len(tag_list)), string_one)
     elif re.match('<[/\w\s]+?>', string_one):
-        tag_list.pop()
+
         string_one.splitlines()
-        print(string_one.rjust(len(tag_list)*12+1, ' '))
+        print(spacer(len(tag_list)), string_one)
+        tag_list.pop()
     elif re.match('[\w\s]+?', string_one):
         for each in string_one.split():
             words_list.append(each)
-        print(spacer(len(tag_list)), words_list)
+            string_string += each
+        #print(spacer(len(tag_list)), words_list)
+        print(spacer(len(tag_list)), string_string)
 
 
